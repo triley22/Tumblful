6b081abca9c897d7110d3475b68fde2cd68fca41
Jason Blanchard <jas.blanchard@gmail.com>
2014-12-08 08:19:01 -0500

    RESTful actions for image_links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Tumblful::Application.routes.draw do
+  get 'text_posts/index'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,6 +9,9 @@ Tumblful::Application.routes.draw do
     resources :follows, :except => [:new, :edit, :show, :update]
     root :to => 'follows#index', :as => :user_root
   end
+
+  resources :image_links
+  resources :text_posts
 
   # You can have the root of your site routed with "root"
   root :to => 'home#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ImageLink.create(url: 'https://pbs.twimg.com/profile_images/378800000577077151/4956a8032dd8549b99463f8501fe88bf_400x400.png', title: 'Thinkful logo')
+ImageLink.create(url: 'http://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Ruby_on_Rails.svg/791px-Ruby_on_Rails.svg.png', title: "Rails logo" )
+ImageLink.create(url: 'https://lh6.googleusercontent.com/-NxZGUZXZg6A/AAAAAAAAAAI/AAAAAAAAAAc/QS4oVJL2NgI/photo.jpg', title: "Ruby logo")
