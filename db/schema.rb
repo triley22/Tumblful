47364a268f1b0158dd32c92a76e6388a01d5348f
Jason Blanchard <jas.blanchard@gmail.com>
2014-12-08 07:50:53 -0500

    Create TextPost model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208124745) do
+ActiveRecord::Schema.define(version: 20141208124943) do
 
   create_table "follows", force: true do |t|
     t.integer  "user_id"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20141208124745) do
   create_table "image_links", force: true do |t|
     t.string   "title"
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "text_posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
